b153b6f6a5be7e49e07feeeda873b5bc905486a9
sdeepanshu11111 <sdeepanshu1111@gmail.com>
2022-02-01 11:18:45 +0530

    mm

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+
 import "./App.css";
+import Navbar from "./components/navbar";
 
 function App() {
+  useEffect(() => {
+    var x = window.matchMedia("(max-width: 600px)");
+
+    console.log("x", x);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">f</header>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/navbar/index.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import { Fade, Slide } from "react-awesome-reveal";
+import "./style.scss";
+import Logo from "./../assets/Icons/logo.svg";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  return (
+    <div className="navbar">
+      <Fade cascade>
+        <img src={Logo} height="100%" alt="" />
+      </Fade>
+
+      <div className="list">
+        <Fade cascade>
+          <div className="under projects">Projects</div>
+          <div className="under about-us">About Us</div>
+          <div className="under contact-us">Contact Us</div>
+        </Fade>
+      </div>
+
+      <div className="hamburger" onClick={() => setNav(!nav)}>
+        <div className="ham1"></div>
+        <div className="ham2"></div>
+        <div className="ham3"></div>
+      </div>
+
+      <div className={`sider ${nav ? "show-sider" : null}`}>
+        <Fade>
+          <div className="sider-logo">
+            <img src={Logo} height="100%" alt="" />
+            <h1 onClick={() => setNav(!nav)}>X</h1>
+          </div>
+        </Fade>
+
+        <div className="sider-list">
+          <Slide delay={0.5}>
+            <div className=" projects">Projects</div>
+          </Slide>
+
+          <Slide delay={1000}>
+            <div className=" about-us">About Us</div>
+          </Slide>
+
+          <Slide delay={1500}>
+            <div className=" contact-us">Contact Us</div>
+          </Slide>
+        </div>
+      </div>
+
+      {/* <img src={MainBg} alt="" /> */}
+    </div>
+  );
+};
+
+export default Navbar;
